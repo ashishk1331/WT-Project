bc87841e025783743b21fbe406cb56dd2b71aa29
Annany Vishwakarma <annany.21211@knit.ac.in>
2024-05-07 12:39:32 +0530

    feature : added update, delete routes and minor changes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,15 +1,15 @@
 import { User } from "../models/user.model.js";
+import pkg from "bcryptjs";
+
+const { hashSync, genSaltSync, compareSync } = pkg;
 
 export async function saveNewUser(req, res) {
-  const { username, password, name, email, year, branch, rollno } = req.body;
+  const { email, password } = req.body;
+
+  const hashedPassword = hashSync(password, genSaltSync(12));
   const newUser = new User({
-    username,
-    password,
-    name,
     email,
-    year,
-    branch,
-    rollno,
+    password: hashedPassword,
   });
   await newUser
     .save()
@@ -21,14 +21,52 @@ export async function saveNewUser(req, res) {
     });
 }
 
-export async function getUser(req, res) {
+export async function loginUser(req, res) {
   const { email, password } = req.body;
   const dbUser = await User.findOne({ email });
   if (!dbUser) {
     return res.status(404).json({ message: "User not found" });
   }
-  if (dbUser.password !== password) {
-    return res.status(401).json({ message: "Unauthorized User" });
+
+  if (!compareSync(password, dbUser.password)) {
+    return res.status(401).json({ message: "Invalid Credentials" });
   }
-  return res.status(200).json({ user: dbUser });
+  const { _id, name, email: em, year, branch, rollno } = dbUser;
+
+  return res
+    .status(200)
+    .json({ user: { _id, name, em, year, branch, rollno } });
+}
+
+export async function deleteUser(req, res) {
+  const { id } = req.params.id;
+  try {
+    await User.findByIdAndDelete(id);
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    return res.status(402).json({ error: error });
+  }
+}
+
+export async function updateUser(req, res) {
+  const { id } = req.params.id;
+  const { name, email, password, rollno, branch, year } = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        email,
+        password,
+        rollno,
+        branch,
+        year,
+      },
+      { new: true }
+    );
+
+    return res.status(200).json({ user: updatedUser });
+  } catch (error) {
+    return res.status(402).json({ error: error });
+  }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { saveNewUser, getUser } from "../controllers/user.js";
+import {
+  saveNewUser,
+  loginUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/signup", saveNewUser);
-router.post("/login", getUser);
+router.post("/login", loginUser);
+router.delete("/delete/:id", deleteUser);
+router.patch("/update/:id", updateUser);
 
 export default router;
